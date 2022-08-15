9fe1e68e8a214d459bd5d8bc4b5e046deeee488d
Jason Hallen <hallenj@Jasons-MacBook-Pro.local>
2022-08-15 12:49:08 -0500

    Formatting comment dates

--- cli.js
+++ cli.js
@@ -277,6 +277,8 @@ async function blogIndex() {
                     commentArticle.getElementsByClassName("comment-author")[0].innerHTML = ymlParsed.name + commentArticle.getElementsByClassName("comment-author")[0].innerHTML
                     commentArticle.getElementsByClassName("comment-link")[0].setAttribute("href", `#comment-${ymlParsed._id}`)
                     commentArticle.getElementsByClassName("comment-link")[0].innerHTML = ymlParsed.date
+                    let testDate = new Date(ymlParsed.date)
+                    console.log(testDate)
                     commentArticle.getElementsByClassName("comment-text")[0].innerHTML = marked.parse(ymlParsed.message)
                     commentListDiv.insertAdjacentElement('beforeend', commentArticle)
                     
