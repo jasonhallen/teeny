63346e7aeab9829b6e1c8fbdd1b0866fe1acb038
Jason Hallen <hallenj@Jasons-MacBook-Pro.local>
2022-08-15 20:31:26 -0500

    debugging

--- cli.js
+++ cli.js
@@ -284,7 +284,7 @@ async function blogIndex() {
                     
                     if (ymlParsed.replying_to_uid) {
                         commentArticle.getElementsByClassName("comment-reply-link")[0].remove()
-                        commentsListDiv.getElementById(ymlParsed.replying_to_uid).after(commentArticle)
+                        commentListDiv.getElementById(ymlParsed.replying_to_uid).after(commentArticle)
                     } else {
                         commentArticle.getElementsByClassName("comment-reply-link")[0].setAttribute("onclick", 
                         `return addComment.moveForm('comment-${ymlParsed._id}', 'comment-form', '${ymlParsed._id}')`)
