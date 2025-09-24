2442d269f3e93776de7a39e7325d01273ad55fc4
Jason Hallen <hallenj@Jasons-MacBook-Pro.local>
2025-09-24 11:13:14 -0500

    coding

--- cli.js
+++ cli.js
@@ -553,9 +553,7 @@ async function rollPage(current_roll) {
 <div id='rolls_navigation' class='muted'>
     <!--${rolls_nav_first}-->
     ${rolls_nav_previous}
-    <span>[&#x2022;</span>
-    <span id='rolls-nav-select' onclick="selectOpen(event)">${current_roll}<span class="dropdown-icon">&#x2228;</span>]</span>
-    <span>&#x2022;</span>
+    <span id='rolls-nav-select' onclick="selectOpen(event)">[${current_roll}<span class="dropdown-icon">&#x2228;</span>]</span>
     ${rolls_nav_next}
     <!--${rolls_nav_last}-->
 </div>
