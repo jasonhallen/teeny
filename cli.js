e86a6f85e95ecfef40b2e6405875ff67d1910f18
Jason Hallen <hallenj@Jasons-MacBook-Pro.local>
2025-09-23 21:03:13 -0500

    coding

--- cli.js
+++ cli.js
@@ -544,7 +544,7 @@ async function rollPage(current_roll) {
     <div id='rolls_navigation'>
         ${rolls_nav_first}
         ${rolls_nav_previous}
-        <span id="rolls_nav_select"></span>
+        <span id='rolls_nav_select'></span>
         ${rolls_nav_next}
         ${rolls_nav_last}
     </div>
