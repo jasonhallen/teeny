3a081d5e2da899d55dc82790d86f78083969e3c0
Jason Hallen <hallenj@Jasons-MacBook-Pro-2.local>
2026-03-18 12:08:50 -0500

    Adding photo roll titles

--- cli.js
+++ cli.js
@@ -200,6 +200,7 @@ async function processPage(pagePath) {
     // Add H2 title
     if (frontmatter.title) {
         if (targetPath === "photo") {
+            document.title = `Roll: ${frontmatter.title}`
             // Insert title with roll dropdown
             let [image_page_header, roll_navigation_string] = await rollPage(frontmatter.title)
             if (frontmatter.film) {
